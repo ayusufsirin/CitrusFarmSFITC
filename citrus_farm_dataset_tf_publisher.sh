6b8bcffefa645c254ccd2760ba36568e198c4ac1
Ahmet Yusuf Şirin <65160352+ayusufsirin@users.noreply.github.com>
2025-07-27 23:55:54 +0300

    Add static transforms

--- citrus_farm_dataset_tf_publisher.sh
+++ citrus_farm_dataset_tf_publisher.sh
@@ -12,12 +12,17 @@ cleanup() {
 # Trap SIGINT (Ctrl+C) and call cleanup
 trap cleanup SIGINT
 
+# https://ucr-robotics.github.io/Citrus-Farm-Dataset/calibration.html
+# TODO: Set transform offsets
 rosrun tf static_transform_publisher 0 0 0 0 0 0 map odom 100&
 python3 odom_to_tf.py&
-rosrun tf2_ros static_transform_publisher 0 0 0.1 0 0 0 base_link zed2i_left_camera_optical_frame&
-rosrun tf2_ros static_transform_publisher 0 0 0.1 0 0 0 base_link velodyne&
-rosrun tf2_ros static_transform_publisher 0 0 0.1 0 0 0 base_link zed2i_base_link&
-rosrun tf2_ros static_transform_publisher 0 0 0.1 0 0 0 zed2i_base_link zed2i_imu_link&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link zed2i_base_link&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link camera_link&
+rosrun tf2_ros static_transform_publisher 0.04 0 0.3787 0 0 0 base_link velodyne&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 odom /jackal_velocity_controller/odom&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 1.57079633 0 base_link microstrain_link&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 zed2i_base_link zed2i_left_camera_optical_frame&
+rosrun tf2_ros static_transform_publisher 0 0 0 0 1.57079633 0 zed2i_base_link zed2i_imu_link&
 
 # Wait for background processes to finish
 wait
